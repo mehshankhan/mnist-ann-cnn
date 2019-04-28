9d66f19d91e9ebdfc8eb0069159a1f1f8c389290
mehshankhan <mehshankhan@ymail.com>
2019-04-29 01:06:49 +0500

    mnist cnn added

---
+++ mnist_cnn/mnistCNN.py
@@ -0,0 +1,69 @@
+import numpy
+import matplotlib.pyplot as plt
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.optimizers import Adam
+from keras.utils import np_utils
+
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# Plot ad hoc mnist instances
+
+# plot 4 images as gray scale
+plt.subplot(221)
+plt.imshow(X_train[0], cmap=plt.get_cmap('gray'))
+plt.subplot(222)
+plt.imshow(X_train[1], cmap=plt.get_cmap('gray'))
+plt.subplot(223)
+plt.imshow(X_train[2], cmap=plt.get_cmap('gray'))
+plt.subplot(224)
+plt.imshow(X_train[3], cmap=plt.get_cmap('gray'))
+# show the plot
+plt.show()
+# Reshaping to format which CNN expects (batch, height, width, channels)
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], 1).astype('float32')
+
+# normalize inputs from 0-255 to 0-1
+X_train/=255
+X_test/=255
+
+# one hot encode
+number_of_classes = 10
+y_train = np_utils.to_categorical(y_train, number_of_classes)
+y_test = np_utils.to_categorical(y_test, number_of_classes)
+
+# create model
+model = Sequential()
+model.add(Conv2D(32, (5, 5), input_shape=(X_train.shape[1], X_train.shape[2], 1), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(number_of_classes, activation='softmax'))
+print(model.summary())
+# Compile model
+model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=200)
+
+# Save the model
+model.save('models/mnistCNN.h5')
+
+# Final evaluation of the model
+metrics = model.evaluate(X_test, y_test, verbose=0)
+print("Metrics(Test loss & Test Accuracy): ")
+print(metrics)
